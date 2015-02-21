882e502270eb2a61186016229f40f279d242da51
Wink Saville <wink@saville.com>
2015-02-20 20:49:45 -0800

    A StateMachine
    Simple state machine that uses an integer as its state and processes
    message synchronously.
    
    I have a simple test which uses cpuTime to test passing a new message
    and allocating a new message each time. Allocating takes about 3000 times
    longer at about 5us per message where as passing the same message takes
    1.7ns.
    
    The takeaway here is that allocating a new message is expensive, and
    that we need to resue them if possible.

--- statemachine.nim
+++ statemachine.nim
@@ -1,1 +1,56 @@
-echo "Hi statemachine"
+import times
+
+type
+  Message = ref object of RootObj
+    cmd: int32
+
+  State = int
+
+  StateMachine = ref object of RootObj
+    curState: int
+
+# processMsg needs to be dynamically dispatched thus its a method
+method processMsg(sm: StateMachine, msg: Message) =
+  echo "StateMachine.processMsg msg=", msg[]
+
+proc transitionTo(sm: StateMachine, nextState: int) =
+  sm.curState = nextState
+
+proc sendMsg*(sm: StateMachine, msg: Message) =
+  sm.processMsg(msg)
+
+when isMainModule:
+  type
+    TestSm = ref object of StateMachine
+      counter1: int
+      counter2: int
+
+  var
+    testSm1: TestSm
+    msg: Message
+
+  method processMsg(sm: TestSm, msg: Message) =
+    case sm.curState
+    of 1:
+      sm.counter1 += 1
+      sm.transitionTo(2)
+    of 2:
+      sm.counter2 += 1
+      sm.transitionTo(1)
+    else: echo "TestSm default state"
+
+  testSm1 = TestSm(curState: 1)
+  msg = Message()
+
+  var startTime = cpuTime()
+  var loops: int32 = 1_000_000_000
+  for cmd in 1i32 .. loops: # better way to make a range of i32 literals?
+    when false:
+      msg = Message(cmd: cmd) # 5,100ns/loop, 3000 time slower
+    else:
+      msg.cmd = cmd # 1.7ns/loop
+    msg.cmd = cmd
+    testSm1.sendMsg(msg)
+  var endTime = cpuTime()
+
+  echo("time=" & $((endTime - startTime)/float(loops)) & " testSm1=" & $testSm1[])
